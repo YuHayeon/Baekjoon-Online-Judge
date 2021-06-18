8c0aae2e961a4c12f678ead7788e047d40ced5ea
YuHayeon <youha0930@gmail.com>
2021-06-19 00:04:48 +0900

    remove dir

--- BOJ/python/3052나머지.py
+++
@@ -1,7 +0,0 @@
-num = set()
-
-for i in range(0,10):
-    x = int(input())
-    num.add(x%42)
-
-print(len(num))
--- BOJ/python/4673셀프넘버.py
+++
@@ -1,6 +0,0 @@
-count = 0
-
-for i in range(100):
-    for j in str(i):
-        count = count + int(j)
-    print(i+count)

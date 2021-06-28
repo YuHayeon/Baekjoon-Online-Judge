93ab4815a11300cd39f97a1de5a47453f977f0e3
YuHayeon <youha0930@gmail.com>
2021-06-28 17:29:06 +0900

    6-3 : 성적이 낮은 순서로 학생 출력하기

---
+++ algorithm/Sorting/6-3.py
@@ -0,0 +1,13 @@
+#Sorting
+#6-3 성적이 낮은 순서로 학생 출력하기
+end = int(input())
+grade = {}
+
+for i in range(end):
+    n, g = input().split()
+    grade.setdefault(n, int(g))
+
+sort = sorted(grade.items(), key = lambda x : x[1])
+
+for item in sort:
+    print(item[0], end=' ')

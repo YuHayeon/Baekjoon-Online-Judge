e5f75335473e47fd30a4f59ef5498af248f8d053
YuHayeon <youha0930@gmail.com>
2021-10-10 11:38:25 +0900

    1904 : 01타일 - 시간단축

--- BOJ/Dynamic-Programming/1904.py
+++ BOJ/Dynamic-Programming/1904.py
@@ -5,11 +5,12 @@ import sys
 n = int(sys.stdin.readline())
 d = [0 for _ in range(n+1)]
 
-d[1] = 1
-if n > 1:
-    d[2] = 2
+a, b = 1, 2
 
 for i in range(3, n+1):
-    d[i] = (d[i-1] + d[i-2]) % 15746
+    a, b = b, (a+b)%15746
 
-print(d[n])
+if n==1:
+    b = 1
+
+print(b)

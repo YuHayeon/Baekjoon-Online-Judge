35036ad4560cc143ca7f450dfa044c94bc702a30
YuHayeon <youha0930@gmail.com>
2021-11-26 00:30:32 +0900

    10773 : 제로

---
+++ BOJ/Data-Structure/10773.py
@@ -0,0 +1,17 @@
+# 10773 : 제로
+
+import sys
+
+n = int(sys.stdin.readline())
+stack = []
+
+for i in range(n):
+    num = int(sys.stdin.readline())
+    if num==0:
+        stack.pop()
+    else:
+        stack.append(num)
+    
+result = sum(stack)
+
+print(result)

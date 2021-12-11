d4cbe8fd54d696077e004b561da55144099b89b3
YuHayeon <youha0930@gmail.com>
2021-12-12 00:31:28 +0900

    11726 : 2xn 타일링

---
+++ BOJ/Dynamic-Programming/11726.py
@@ -0,0 +1,12 @@
+# 11726 : 2xn 타일링
+
+import sys
+
+n = int(sys.stdin.readline())
+d = [0, 1, 2] + [0]*n
+
+if n>2:
+    for i in range(3, n+1):
+        d[i] = d[i-1] + d[i-2]
+ans = d[n]%100007
+print(ans)

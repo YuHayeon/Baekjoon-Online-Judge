171c5782422ec21028cb4cc209cede94791e9aab
YuHayeon <youha0930@gmail.com>
2021-12-17 12:48:56 +0900

    11723 : 집합 - dictionary

--- BOJ/Implementation/11723.py
+++ BOJ/Implementation/11723.py
@@ -1,28 +1,37 @@
 # 11723 : 집합
 
 import sys
-bit = 0
-def set_operation(op, num):
-    global bit
-    if op == 'add':
-        bit = bit | (1<<num)
-    elif op == 'remove':
-        bit = bit & ~(1<<num)
-    elif op == 'check':
-        if bit & (1<<num) == 0:
-            print(0)
-        else: print(1)
-    elif op == 'toggle':
-        bit = bit ^ (1<<num)
 
+dict = {}
 t = int(sys.stdin.readline())
-for _ in range(t):
+
+for num in range(t):
     op = sys.stdin.readline().rstrip()
 
     if op == 'all':
-        bit = (1 << 20) -1
+        for i in range(1, 21):
+            dict[i] = i
+
     elif op == 'empty':
-        bit = 0
+        dict = {}
+
     else:
         op, num = op.split()
-        set_operation(op, int(num)-1)
+        num = int(num)
+        if op == 'add':
+            dict[num] = num
+        elif op == 'remove':
+            if dict.get(num)==num:
+                del dict[num]
+            else:
+                continue
+        elif op == 'check':
+            if dict.get(num)==num:
+                print(1)
+            else:
+                print(0)
+        elif op == 'toggle':
+            if dict.get(num)==num:
+                del dict[num]
+            else:
+                dict[num]=num

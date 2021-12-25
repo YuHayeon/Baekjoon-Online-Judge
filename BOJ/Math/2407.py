56c4c2eb0ca4cad460ba7f22701fbdc0e9a0360b
YuHayeon <youha0930@gmail.com>
2021-12-25 10:36:28 +0900

    2407 : 조합

---
+++ BOJ/Math/2407.py
@@ -0,0 +1,14 @@
+# 2407 : 조합
+
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+d = [[1, 1] for _ in range(m+1)]
+result = 0
+
+for i in range(1, m+1):
+    d[i][0] = d[i-1][0] * (n-i+1)
+    d[i][1] = d[i-1][1] * i
+
+result = (d[i][0]//d[i][1])
+print(result)

043b764e013594afcbf902ce0dc5d3234efb5435
YuHayeon <youha0930@gmail.com>
2021-12-27 21:53:57 +0900

    15650 : N과 M (2) - 스터디

--- BOJ/Backtracking/15650.py
+++ BOJ/Backtracking/15650.py
@@ -1,19 +1,19 @@
 # 15650 : N과 M (2)
 
 import sys
+input = sys.stdin.readline
 
-n, m = map(int, sys.stdin.readline().split())
+n, m = map(int, input().split())
 s = []
 
 def back(start):
     if len(s) == m:
         print(' '.join(map(str, s)))
-
     for i in range(start, n+1):
         if i in s:
             continue
         s.append(i)
-        back(i+1)
+        back(i)
         s.pop()
 
 back(1)

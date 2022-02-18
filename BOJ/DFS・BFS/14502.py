505bd232cb6c1de1666ca3f933d63c765879f641
YuHayeon <youha0930@gmail.com>
2022-02-18 22:09:32 +0900

    14502 : 연구소 - pypy 성공

--- BOJ/DFS・BFS/14502.py
+++ BOJ/DFS・BFS/14502.py
@@ -1,10 +1,10 @@
 from collections import deque
+
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-wall_list = []
-result = 0
 
 d = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+result = 0
 
 
 def bfs(x, y, lab):
@@ -31,11 +31,13 @@ def wall(wall_cnt):
         lab = [x[:] for x in graph]  # 깊은 복사
         cnt = 0
 
+        # 바이러스(2)일 때, bfs 실행
         for i in range(n):
             for j in range(m):
                 if lab[i][j] == 2:
                     bfs(i, j, lab)
 
+        # 안전지대(0) 개수 카운트
         for i in lab:
             for j in i:
                 if j == 0:
@@ -44,9 +46,9 @@ def wall(wall_cnt):
         result = max(result, cnt)
         return
 
+    # 백트래킹을 이용해 벽을 세움
     for i in range(n):
         for j in range(m):
-
             if graph[i][j] == 0:
                 graph[i][j] = 1
                 wall(wall_cnt + 1)

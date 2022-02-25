c828b589dc3c38e5a5027dc7fbc9c2d89963a63b
YuHayeon <youha0930@gmail.com>
2022-02-25 11:05:29 +0900

    delete

--- BOJ/Implementation/11021.py
+++
@@ -1,6 +0,0 @@
-import sys
-input = sys.stdin.readline
-n = int(input())
-for i in range(1,n+1):
-    a,b = map(int, input().split())
-    print('Case #{0}: {1}'.format(i, a+b))
--- BOJ/README.md
+++
@@ -1,3 +0,0 @@
-# baekjoon-algorithm
-
-[![solved.ac tier](http://mazassumnida.wtf/api/v2/generate_badge?boj=hayu)](https://solved.ac/hayu)
